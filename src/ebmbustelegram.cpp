ffab6722ee465774430bab890da3f0883949d142
Peter Diener <diener.git@sme-gmbh.com>
2018-05-02 00:04:11 +0200

    Corrected wrong initialization of telegram id in overloaded constructor of EbmBusTelegram.

--- src/ebmbustelegram.cpp
+++ src/ebmbustelegram.cpp
@@ -15,9 +15,11 @@ EbmBusTelegram::EbmBusTelegram()
 
 EbmBusTelegram::EbmBusTelegram(EbmBusCommand::Command command, quint8 fanAddress, quint8 fanGroup, QByteArray data, bool servicebit)
 {
-    static quint64 id = 0;
+    static quint64 id = 1;  // Start counting telegram id with 1. 0 is reserved for error
     this->m_id = id;
     id++;
+    if (id == 0)            // Wrap around and avoid 0 - if that might ever happen with quint64... - just to be correct.
+        id = 1;
 
     this->command = command;
     this->fanAddress = fanAddress;
